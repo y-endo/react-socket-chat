49dae3492b1f740a0cca6e5d1a1b34449552823c
YukiEndo <yuki.a.endo@imjp.co.jp>
2020-03-11 19:03:40 +0900

    [add]モーダルの開閉にアニメーション追加
    react-spring導入

--- src/ts/pages/Index.tsx
+++ src/ts/pages/Index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import { useTransition, animated } from 'react-spring';
 import queryRooms from '~/graphql/queries/rooms.graphql';
 import Layout from '~/ts/layouts/default';
 import RoomList from '~/ts/components/RoomList';
@@ -8,6 +9,11 @@ import Modal from '~/ts/components/Modal';
 
 const Index: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const transition = useTransition(isModalOpen, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
   const { loading, error, data } = useQuery(
     gql`
       ${queryRooms}
@@ -33,7 +39,16 @@ const Index: React.FC = () => {
       <>
         <button onClick={handleButtonClick}>チャットルーム作成</button>
         <RoomList rooms={data.rooms} />
-        {isModalOpen && <Modal closeModal={closeModal} />}
+        <>
+          {transition.map(
+            ({ item, key, props }) =>
+              item && (
+                <animated.div style={props} key={key}>
+                  <Modal closeModal={closeModal} />
+                </animated.div>
+              )
+          )}
+        </>
       </>
     );
     return <Layout content={content} />;
